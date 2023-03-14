1a8451302b61f5cc4ae9456f54df5451f4c2efb8
Vladimir RM <shvdrummer@gmail.com>
2023-03-14 09:22:11 +0200

    update map

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 const generateAutoCards = (brand, model) => {
   return `
   <div class='auto'>
-  <h1></h1>
-  <p></p>
+  <h1>1</h1>
+  <p>1</p>
   </div>
   `;
 };
 const div = document.createElement("div");
 div.classList.add("auto");
 
- const body = 
+const body = document.querySelector("body");
+
+body.appendChild;
 const listAuto = () => [
   { brand: "Toyota", model: "Camry" },
   { brand: "Lexus", model: "Es 350" },
