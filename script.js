60d6285e20903096f4c5ec93f0bac8d759f798f2
Vladimir RM <shvdrummer@gmail.com>
2023-03-14 15:16:44 +0200

    save copy all code

--- script.js
+++ script.js
@@ -1,22 +1,86 @@
-const generateAutoCards = (brand, model) => {
-  return `
-  <div class='auto'>
-  <h1>1</h1>
-  <p>1</p>
-  </div>
-  `;
-};
-const carDiv = document.createElement("div");
-div.classList.add("auto");
+const div = document.createElement("div");
+
+div.classList.add("wrapper");
 
 const body = document.querySelector("body");
+body.appendChild(div);
 
-body.appendChild;
-const listAuto = () => [
-  { brand: "Toyota", model: "Camry" },
-  { brand: "Lexus", model: "Es 350" },
+const header = document.createElement("h1");
+header.textContent = " DOM (Document object model)";
+
+console.log(header);
+
+div.insertAdjacentElement("beforebegin", header);
+
+const ul = `
+       <ul>
+       <li>one</li>
+       <li>two</li>
+       <li>three</li>
+       </ul>
+`;
+div.innerHTML = ul;
+
+const img = document.createElement("img");
+
+img.src =
+  "https://fastly.picsum.photos/id/862/240/240.jpg?hmac=o-z7CteQz19BKng4_1BnHfIivj3TjDohkw5YJ9o1Bok";
+img.width = 240;
+img.classList.add("super");
+img.alt = "Super man";
+
+div.appendChild(img);
+
+const elemHtml = `
+<div class='pDiv'>
+<p>Paragraph 1</p>
+<p>Paragraph 2</p>
+</div>
+`;
+const ulList = div.querySelector("ul");
+ulList.insertAdjacentHTML("beforebegin", elemHtml);
+
+const pDiv = document.querySelector(".pDiv");
+pDiv.children[1].classList.add("text");
+pDiv.firstElementChild.remove();
+console.log(pDiv.children);
+
+/////////////////////////////////////////////////////
+
+const generateAutoCard = (brand, color, year) => {
+  return `<div class='autoCard'>
+      <h2>${brand.toUpperCase()}  ${year}</h2>
+      <p> автомобиль  ${brand.toUpperCase()} - ${year}   года. Возвраст авто YEAR лет  </p>
+      <p> цвет: ${color}</p>
+      <button type='button' class='btn'>Delete</button>
+      </div>`;
+};
+
+const carsDiv = document.createElement("div");
+carsDiv.classList.add("autos");
+
+const carsList = [
+  { brand: "Toyota", color: "red", year: 2020 },
+  { brand: "Bmw", color: "black", year: 2021 },
+  { brand: "Lexus", color: "green", year: 2019 },
 ];
 
-const listHtml = listAuto.map((car) => {
-  return generateAutoCards(car.brand, car.model);
+const carsHtml = carsList
+  .map((car) => {
+    return generateAutoCard(car.brand, car.color, car.year);
+  })
+  .join();
+
+carsDiv.innerHTML = carsHtml;
+div.insertAdjacentElement("beforebegin", carsDiv);
+
+const buttons = document.querySelectorAll(".btn");
+const handleClick = (e) => {
+  const currentButton = e.currentTarget;
+  currentButton.closest(".autoCard").remove();
+};
+
+buttons.forEach((button) => {
+  button.addEventListener("click", handleClick);
 });
+console.log(carsList);
--- script1.js
+++ script1.js
@@ -1,34 +1,69 @@
-const div = document.createElement("div");
-div.classList.add("wrapper");
+// // const div = document.createElement("div");
+// // div.classList.add("wrapper");
 
-const body = document.querySelector("body");
-body.appendChild(div);
+// // const body = document.querySelector("body");
+// // body.appendChild(div);
 
-const generateAutoCard = (brand, color, year) => {
-  console.log( brand)
-  return `<div class='autoCard'>
-    <h2>${brand.toUpperCase}${year}</h2>
-    <p> автомобиль  ${brand.toUpperCase} - ${year}  года. Возвраст авто YEAR лет  </p>
-    <p> цвет: ${color}</p>
-    </div>`;
-};
+// // const generateAutoCard = (brand, color, year) => {
+// //   console.log( brand)
+// //   return `<div class='autoCard'>
+// //     <h2>${brand.toUpperCase}${year}</h2>
+// //     <p> автомобиль  ${brand.toUpperCase} - ${year}  года. Возвраст авто YEAR лет  </p>
+// //     <p> цвет: ${color}</p>
+// //     </div>`;
+// // };
 
-const carsDiv = document.createElement("div");
-carsDiv.classList.add("autos");
+// // const carsDiv = document.createElement("div");
+// // carsDiv.classList.add("autos");
 
-const carsList = [
-  { brand: "Toyota", color: "red", year: 2020 },
-  { brand: "Bmw", color: "black", year: 2021 },
-  { brand: "Lexus", color: "green", year: 2019 },
-];
-// console.log(carsList);
+// // const carsList = [
+// //   { brand: "Toyota", color: "red", year: 2020 },
+// //   { brand: "Bmw", color: "black", year: 2021 },
+// //   { brand: "Lexus", color: "green", year: 2019 },
+// // ];
+// // // console.log(carsList);
 
-const carsHTML = carsList
-  .map((car) => {
-    return generateAutoCard(car.brand, car.color, car.year);
-  })
-  // .join("");
+// // const carsHTML = carsList
+// //   .map((car) => {
+// //     return generateAutoCard(car.brand, car.color, car.year);
+// //   })
+// //   // .join("");
 
-carsDiv.innerHTML = carsHTML;
-div.insertAdjacentElement("beforebegin", carsDiv);
-console.log(carsDiv);
+// // carsDiv.innerHTML = carsHTML;
+// // div.insertAdjacentElement("beforebegin", carsDiv);
+// // console.log(carsDiv);
+
+
+
+
+
+
+
+
+// const generateAutoCards = (brand, model) => {
+//     console.log(brand);
+//     return `
+//     <div class='auto'>
+//     <h1>${brand}</h1>
+//     <p>${model}</p>
+//     </div>
+//     `;
+//   };
+//   const carDiv = document.createElement("div");
+//   carDiv.classList.add("auto");
+  
+//   const body = document.querySelector("body");
+  
+//   body.appendChild(carDiv);
+//   const listAuto = () => [
+//     { brand: "Toyota", model: "Camry" },
+//     { brand: "Lexus", model: "Es 350" },
+//   ];
+  
+//   const listHtml = listAuto.map((car) => {
+//     return generateAutoCards(car.brand, car.model);
+//   });
+  
+//   carDiv.innerHTML = listHtml;
+//   carDiv.appendChild(listHtml);
+  
