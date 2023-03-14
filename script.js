ff4d5c95e32447ffbe6582d4027d84f61a2d0717
Vladimir RM <shvdrummer@gmail.com>
2023-03-14 09:12:16 +0200

    create render list car

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const genarateAutoCards = (brand, model) => {
+const generateAutoCards = (brand, model) => {
   return `
   <div class='auto'>
   <h1></h1>
@@ -10,6 +10,9 @@ const genarateAutoCards = (brand, model) => {
 const listAuto = ()=>[
 { brand: 'Toyota' ,model: 'Camry'},
 { brand: 'Lexus' ,model: 'Es 350'}
-
-
 ]
+
+
+const listHtml = listAuto.map(car =>{
+  return generateAutoCards(car.brand,car.model)
+})
