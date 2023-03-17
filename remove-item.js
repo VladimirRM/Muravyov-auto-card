055e1d99c4637526435644ff4ddc1e3d352eec98
Vladimir RM <shvdrummer@gmail.com>
2023-03-17 12:33:30 +0200

    create markup for list

--- remove-item.js
+++ remove-item.js
@@ -4,4 +4,20 @@ div.classList.add("wrapper");
 const body = document.body;
 body.appendChild(div);
 
+const generateAutoCard = (brand, model) => {
+  return `
+    <div class'autoCards'>
+    <ul>
+    <li>Brand</li>
+    <li>Model</li>
+    </ul>
+    </div>
+    `;
+};
+
+const listAuto = [
+    { brand: "Toyota", model: "Camry" },
+    { brand: "Lexus", model: "Es 350" },
+  ];
+
 console.log(div);
