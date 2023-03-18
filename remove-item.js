9cd821ce8bea7ae5c24acbcbce471573980e8616
Vladimir RM <shvdrummer@gmail.com>
2023-03-18 09:38:37 +0200

    Save point

--- remove-item.js
+++ remove-item.js
@@ -6,18 +6,38 @@ body.appendChild(div);
 
 const generateAutoCard = (brand, model) => {
   return `
-    <div class'autoCards'>
-    <ul>
-    <li>Brand</li>
-    <li>Model</li>
+    <div class'autoCard'>
+    <ul class='ul'>
+    <li class='one'>${brand}</li>
+    <li>${model}</li>
     </ul>
+    <button class='btn'>Delete</button>
     </div>
     `;
 };
 
 const listAuto = [
-    { brand: "Toyota", model: "Camry" },
-    { brand: "Lexus", model: "Es 350" },
-  ];
+  { brand: "Toyota", model: "Camry" },
+  { brand: "Lexus", model: "Es 350" },
+];
 
-console.log(div);
+const carsHtml = listAuto
+  .map((car) => {
+    return generateAutoCard(car.brand, car.model);
+  })
+  .join("");
+// div.innerHTML = carsHtml;
+div.innerHTML = carsHtml;
+
+const handleClick = (e) => {
+  const currentButton = e.currentTarget;
+  // currentButton.parentElement.remove();
+//   currentButton.closest(".one").remove();
+currentButton.removeChild('li')
+};
+
+
+const buttons = document.querySelectorAll(".btn");
+buttons.forEach((button) => {
+  button.addEventListener("click", handleClick);
+});
