952b4b3c00d99230c0266ef1fc257880cbb65d12
Vladimir RM <shvdrummer@gmail.com>
2023-03-19 11:49:02 +0200

    create main div for body

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
+const div = document.createElement("div");
+div.classList.add("mainDiv");
+
+document.body.append(div);
+
+
+
 const generateAutoCard = (brand, model) => {
-    return `
+  return `
     <div class='autoCard'>
     <ul class='ul'>
     <li>Brand</li>
     <li>Model</li>
     </ul>
     </div>
-    `
+    `;
 };
 
+const listAuto = [
+  { brand: "Toyota", model: "Camry" },
+  { brand: "Lexus", model: "Es 350" },
+];
 
+const carsHtml = listAuto.forEach((car) => {
+  return generateAutoCard(car.brand, car.model);
+});
