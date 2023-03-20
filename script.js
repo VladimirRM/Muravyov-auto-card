63f1d02b489294eb2d1c420ef7104e1a0e60179c
Vladimir RM <shvdrummer@gmail.com>
2023-03-20 14:02:48 +0200

    crate main div wrapper

--- script.js
+++ script.js
@@ -1,41 +1,7 @@
-const div = document.createElement("div");
-div.classList.add("wrapper");
+ const div = document.createElement('div')
+ div.classList('wrapper')
 
-document.body.append(div);
 
-const generateAutoCard = (brand, model) => {
-  return `
-    <div class='autoCard'>
-    <ul class='ul'>
-    <li>${brand}</li>
-    <li>${model}</li>
-    </ul>
-    <button class='btn'>Delete</button>
-    </div>
-    `;
-};
+ div.appdocument.body;
 
-const listAuto = [
-  { brand: "Toyota", model: "Camry" },
-  { brand: "Lexus", model: "Es 350" },
-];
-// console.log(listAuto);
-
-const carsHtml = listAuto
-  .map((car) => {
-    return generateAutoCard(car.brand, car.model);
-  })
-  .join();
-
-div.innerHTML = carsHtml;
-
-const handlerDelete = (e) => {
-  const currentButton = e.currentTarget;
-  currentButton.parentElement.remove();
-};
-
-const buttons = document.querySelectorAll(".btn");
-buttons.forEach((button) => {
-  button.addEventListener("click", handlerDelete);
-});
-console.log(buttons);
+ console.log(div);
