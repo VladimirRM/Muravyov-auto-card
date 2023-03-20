812b6f0c20a43daca01e34afe59b704bd2bbc7a0
Vladimir RM <shvdrummer@gmail.com>
2023-03-20 14:09:25 +0200

    Create markup

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
- const div = document.createElement('div')
- div.classList('wrapper')
+const div = document.createElement("div");
+div.classList.add("wrapper");
 
+const body = document.body;
+body.appendChild(div);
 
- div.appdocument.body;
+console.log(div);
 
- console.log(div);
+const generateAutoCard = (brand, model) => {
+  return `
+    <div class='autoCard'>
+    <ul>
+    <li>${brand}</li>
+    <li>${model}</li>
+
+    </ul>
+    </div>
+    `;
+};
